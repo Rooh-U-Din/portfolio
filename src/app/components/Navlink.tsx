2d389c45666deaf0c97e694380eb4af5d9b0748f
Rooh-U-Din <fidajokhio2@gmail.com>
2024-12-26 01:51:51 -0800

    first commit

--- src/app/components/Navlink.tsx
+++ src/app/components/Navlink.tsx
@@ -1,29 +1,14 @@
-// import Link from 'next/link'
-// import React from 'react'
+import Link from 'next/link'
+import React from 'react'
 
-// function Navlink({href, title}:any) {
-//   return (
-//     <div>
-//       <Link href={href} className="block py-2 pl text-gray-100 sm:text-xl rounded md:p-0 hover:text-white">
-//       {title}</Link>
-//     </div>
-//   )
-// }
-
-// export default Navlink
-
-
-import Link from "next/link";
-
-const NavLink = ({ href, title }) => {
+function Navlink({href, title}:any) {
   return (
-    <Link
-      href={href}
-      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
-    >
-      {title}
-    </Link>
-  );
-};
+    <div>
+      <Link href={href} className="block py-2 pl text-gray-100 sm:text-xl rounded md:p-0 hover:text-white">
+      {title}</Link>
+    </div>
+  )
+}
 
-export default NavLink;
+export default Navlink
+
